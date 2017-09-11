5aafbd2762a8b342e02f68d4e8def7d4caa95ef4
hsjoihs <hs.ioling.hs@gmail.com>
2017-09-11 16:03:29 +0900

    Memory is Data.Map.Map Word32 Word8, not Data.Map.Map Word32 Word32

--- assembler/Memory.hs
+++ assembler/Memory.hs
@@ -3,6 +3,8 @@ module Memory
 ,writeM
 ,readM
 ,emptyM
+,readByte
+,writeByte
 ,unM
 ,runState,execState,evalState
 ) where
@@ -10,8 +12,23 @@ import qualified Data.Map as M
 import Data.Word
 import Control.Monad.State
 import System.Random
+import Data.Bits
 
-data Memory = Memory {unM :: M.Map Word32 Word32, garbages :: [(Word32,Word32)]} deriving(Show, Eq, Ord)
+
+decompose :: Word32 -> (Word8, Word8, Word8, Word8)
+decompose a = 
+ (fromIntegral (a `shift` (-24))
+ ,fromIntegral (a `shift` (-16))
+ ,fromIntegral (a `shift` (- 8))
+ ,fromIntegral a)
+
+compose :: (Word8, Word8, Word8, Word8) -> Word32
+compose (a,b,c,d) = f a 24 + f b 16 + f c 8 + f d 0
+ where
+  f :: Word8 -> Int -> Word32
+  f t i = ((fromIntegral t)::Word32) `shift` i
+
+data Memory = Memory {unM :: M.Map Word32 Word8, garbages :: [(Word32,Word8)]} deriving(Show, Eq, Ord)
 
 emptyM :: Memory
 emptyM = Memory M.empty []
@@ -19,16 +36,35 @@ emptyM = Memory M.empty []
 
 writeM :: Word32 -> Word32 -> State Memory ()
 writeM addr dat = do
+ let (a,b,c,d) = decompose dat
  Memory m gs <- get
- put $ Memory (M.insert addr dat m) gs
+ writeByte  addr    a
+ writeByte (addr+1) b
+ writeByte (addr+2) c
+ writeByte (addr+3) d
+
+writeByte :: Word32 -> Word8 -> State Memory ()
+writeByte addr dat = do
+ Memory m gs <- get
+ put $ Memory (M.insert addr dat m) gs 
 
 readM :: Word32 -> State Memory Word32
 readM addr = do
+ a <- readByte  addr
+ b <- readByte (addr + 1)
+ c <- readByte (addr + 2)
+ d <- readByte (addr + 3)
+ return $ compose (a,b,c,d)
+
+
+readByte :: Word32 -> State Memory Word8
+readByte addr = do
  Memory m gs <- get
  case M.lookup addr m of
   Just dat -> return dat
   Nothing -> do
    let garbage = fst . random . mkStdGen . fromIntegral $ addr -- make random-like data from addr info
    put $ Memory m ((addr,garbage):gs)
-   writeM addr garbage
+   writeByte addr garbage
    return garbage
+
