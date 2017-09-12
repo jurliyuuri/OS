ef21d4a9e9e740a9c47effd1cefeadab8f7cb02b
hsjoihs <hs.ioling.hs@gmail.com>
2017-09-13 08:37:20 +0900

    Rename function and label in c

--- quicksort.c
+++ quicksort.c
@@ -4,14 +4,14 @@ int tmp;
 
 #define qss(fi, st, ir) (tmp = (st), (st) = (ir), (fi) = tmp)
 
-void quicksort(int* const a, int l, int r)
+void ycax(int* const a, int l, int r)
 {
 	register int* const f0 = a;
 	register int f1 = l; 
 	register int f2 = r;
 	register int f3;
 	if(f1 >= f2) {
-		goto fin;
+		goto lus;
 	}
 	f3 = f1;
 	f3++;
@@ -22,7 +22,7 @@ panqa:
 	}
 	f2 = l;
 	f2 = f0[f2];
-	if(f0[f3] >= f2) {
+	if(f2 <= f0[f3]) {
 		goto iska;
 	}
 	f1++;
@@ -36,9 +36,9 @@ fistir:
 	qss(f0[f1],f0[f2],f0[f1]);
 
 	int p = f1;
-	quicksort(a,l,p-1);
-	quicksort(a,p+1,r);
-fin:	
+	ycax(a,l,p-1);
+	ycax(a,p+1,r);
+lus:	
 	return;	
 }
 
@@ -46,7 +46,7 @@ int dat[] = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8};
 
 int main()
 {
-	quicksort(dat,0,18);
+	ycax(dat,0,18);
 	for(int i=0;i<=18;i++){
 		printf("%d, ",dat[i]);
 	}
