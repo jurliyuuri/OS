ad224e94deb02b884bd558aa036ed50d18e3a7b7
hsjoihs <hs.ioling.hs@gmail.com>
2017-09-12 16:41:33 +0900

    apply tentativeLoad to programs in main

--- assembler/main.hs
+++ assembler/main.hs
@@ -8,11 +8,11 @@ import TentativeLoad
 main :: IO ()
 main = do
  putStrLn "\nfib_non_recursive:"
- print $ fullParse program
+ print $ toTentativeLoad <$> fullParse program
  putStrLn "\nfib_recursive:"
- print $ fullParse program2
+ print $ toTentativeLoad <$> fullParse program2
  putStrLn "\nqsort:"
- print $ fullParse program3
+ print $ toTentativeLoad <$> fullParse program3
 
 program :: String
 program = "'c'i  krz f0 f5 + 4 @  krz f1 0  krz f2 1  fi f0 0 clo  l' is  malkrz xx ka   nta f0 1  krz f3 f1  ata f3 f2  inj f1 f2 f3  krz xx is  krz f0 f1  l' ka  krz xx f5@"
