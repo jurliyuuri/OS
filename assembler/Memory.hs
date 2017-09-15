308791f2347a44eed85c4894fd66a5230645ae59
hsjoihs <hs.ioling.hs@gmail.com>
2017-09-16 07:11:44 +0900

    use to32 in Memory's show

--- assembler/Memory.hs
+++ assembler/Memory.hs
@@ -29,35 +29,11 @@ compose (a,b,c,d) = f a 24 + f b 16 + f c 8 + f d 0
   f :: Word8 -> Int -> Word32
   f t i = (fromIntegral t::Word32) `shift` i
 
-data Memory = Memory {unM :: M.Map Word32 Word8, garbages :: [(Word32,Word8)]} deriving(Show, Eq, Ord)
+data Memory = Memory {unM :: M.Map Word32 Word8, garbages :: [(Word32,Word8)]} deriving(Eq, Ord)
 
 emptyM :: Memory
 emptyM = Memory M.empty []
 
-type Foo = (Maybe Word8, Maybe Word8, Maybe Word8, Maybe Word8)
-
-to32 :: M.Map Word32 Word8 -> M.Map Word32 (Maybe Word32)
-to32 = fmap baz . M.fromListWith foo . map bar . M.toList
- where
-  bar :: (Word32, Word8) -> (Word32, Foo)
-  bar (w32, w8)
-   | w32 `mod` 4 == 0 = (w32', (Just w8, Nothing, Nothing, Nothing))
-   | w32 `mod` 4 == 1 = (w32', (Nothing, Just w8, Nothing, Nothing))
-   | w32 `mod` 4 == 2 = (w32', (Nothing, Nothing, Just w8, Nothing))
-   | otherwise        = (w32', (Nothing, Nothing, Nothing, Just w8))
-   where w32' = (w32 `div` 4)*4
-  foo :: Foo -> Foo -> Foo
-  foo (a,b,c,d) (w,x,y,z) = (m a w, m b x, m c y, m d z)
-  m :: Maybe a -> Maybe a -> Maybe a
-  m Nothing b = b
-  m a _ = a
-  baz :: Foo -> Maybe Word32
-  baz (a,b,c,d) = do
-   w <- a
-   x <- b
-   y <- c
-   z <- d
-   return $ compose (w,x,y,z)
 
 
 
@@ -93,4 +69,41 @@ readByte addr = do
    put $ Memory m ((addr,garbage):gs)
    writeByte addr garbage
    return garbage
+
+
+
+instance Show Memory where
+ show Memory{unM = a, garbages = b} = 
+  "\nMemory: \n" ++ toStr (to32 a) ++ 
+  "\nGarbages: \n" ++ toStr (to32(M.fromList b))
+
+type Foo = (Maybe Word8, Maybe Word8, Maybe Word8, Maybe Word8)
+
+toStr :: M.Map Word32 (Maybe Word32) -> String
+toStr = unlines . map foobar . M.toList
+ where
+  foobar (a,b) = ('\t':) $ show a ++ ": " ++ case b of {Just q -> show q; Nothing -> "*"}
+
+to32 :: M.Map Word32 Word8 -> M.Map Word32 (Maybe Word32)
+to32 = fmap baz . M.fromListWith foo . map bar . M.toList
+ where
+  bar :: (Word32, Word8) -> (Word32, Foo)
+  bar (w32, w8)
+   | w32 `mod` 4 == 0 = (w32', (Just w8, Nothing, Nothing, Nothing))
+   | w32 `mod` 4 == 1 = (w32', (Nothing, Just w8, Nothing, Nothing))
+   | w32 `mod` 4 == 2 = (w32', (Nothing, Nothing, Just w8, Nothing))
+   | otherwise        = (w32', (Nothing, Nothing, Nothing, Just w8))
+   where w32' = (w32 `div` 4)*4
+  foo :: Foo -> Foo -> Foo
+  foo (a,b,c,d) (w,x,y,z) = (m a w, m b x, m c y, m d z)
+  m :: Maybe a -> Maybe a -> Maybe a
+  m Nothing b = b
+  m a _ = a
+  baz :: Foo -> Maybe Word32
+  baz (a,b,c,d) = do
+   w <- a
+   x <- b
+   y <- c
+   z <- d
+   return $ compose (w,x,y,z)
 
