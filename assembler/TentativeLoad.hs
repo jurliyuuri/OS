eff34e7c94423c64629bf1b0a8d9bfe31d0def38
hsjoihs <hs.ioling.hs@gmail.com>
2017-10-03 06:11:44 +0900

    Write "duplicating local label(s)"

--- assembler/TentativeLoad.hs
+++ assembler/TentativeLoad.hs
@@ -23,7 +23,7 @@ toTentativeLoad initAddress arr = do
  let list = concatMap (\(bs,d) -> zip bs $ repeat d) raw2
  case detectDuplicate (map fst list) of
   [] -> return TentativeLoad {tentativeAddressTable = M.fromList raw1, labelTable = M.fromList list}
-  labels -> Left $ LinkError $ "duplicating label(s): " ++ intercalate ", " (map unLabel labels)
+  labels -> Left $ LinkError $ "duplicating local label(s): " ++ intercalate ", " (map unLabel labels)
  
  where 
   (raw1, raw2) = unzip $ zipWith3 f ts (tail ts) arr
