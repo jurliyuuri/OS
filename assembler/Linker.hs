45852111dbcb2a0afe2e02fec376f6495b7079fb
hsjoihs <hs.ioling.hs@gmail.com>
2017-10-02 10:45:14 +0900

    Make Program a complete blackbox

--- assembler/Linker.hs
+++ assembler/Linker.hs
@@ -11,19 +11,19 @@ import Parse
 import TentativeLoad
 import qualified Data.Map as M
 
-type Program = TentativeLoad
+data Program = Program TentativeLoad
 
 
 -- type ParsedFile = ([(Instruction, [Label])],([KueInfo],[Label]))
 linker :: [ParsedFile] -> Either String Program
-linker [(ils, ([], []))] = toTentativeLoad initialAddress ils
+linker [(ils, ([], []))] = Program <$> toTentativeLoad initialAddress ils
 linker _ = error "Linking multiple files is not yet implemented"
 
 initialAddress :: Word32
 initialAddress = 0x1482e8d4 -- just a random value with no meaning
 
 resolveLabel' :: Program -> Label -> Maybe Word32
-resolveLabel' program label = let lt = labelTable program in M.lookup label lt
+resolveLabel' (Program ttl) label = let lt = labelTable ttl in M.lookup label lt
 
 readNX :: Program -> Word32 -> Maybe (Word32, Instruction)
-readNX program currentNX = M.lookup currentNX (tentativeAddressTable program)
+readNX (Program ttl) currentNX = M.lookup currentNX (tentativeAddressTable ttl)
