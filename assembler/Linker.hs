1a418d86dc82a547d6e0385b824dc7db10efd5bd
hsjoihs <hs.ioling.hs@gmail.com>
2017-10-02 09:59:44 +0900

    Move initialAddress to Linker.hs

--- assembler/Linker.hs
+++ assembler/Linker.hs
@@ -13,5 +13,8 @@ type Program = TentativeLoad
 
 -- type ParsedFile = ([(Instruction, [Label])],([KueInfo],[Label]))
 linker :: [ParsedFile] -> Either String Program
-linker [(ils, ([], []))] = toTentativeLoad ils
+linker [(ils, ([], []))] = toTentativeLoad initialAddress ils
 linker _ = error "Linking multiple files is not yet implemented"
+
+initialAddress :: Word32
+initialAddress = 0x1482e8d4 -- just a random value with no meaning
