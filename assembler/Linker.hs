6f79eb128aafd5b924a1fef28eb25519006e2107
hsjoihs <hs.ioling.hs@gmail.com>
2017-10-05 13:27:50 +0900

    get NX info

--- assembler/Linker.hs
+++ assembler/Linker.hs
@@ -13,6 +13,7 @@ import TentativeLoad
 import qualified Data.Map as M
 -- type ParsedFile = ([(Instruction, [Label])],([KueInfo],[Label]))
 
+type PageId = Int
 
 linker' :: [ParsedFile] -> Either LinkError Program'
 linker' pfs = case fromListNoDup $ zipWith assignInts pfs [1..] of
@@ -21,21 +22,20 @@ linker' pfs = case fromListNoDup $ zipWith assignInts pfs [1..] of
   Nothing -> Left $ LinkError "all files have `kue`"
   _ -> do
    loadeds' <- M.traverseWithKey loadWithInt dat
-   let loadeds = map snd $ M.toAscList loadeds' -- [(TentativeLoad, ([Label], [Label])]                              
-   undefined
+   return $ Program' loadeds'
 
-loadWithInt :: Int -> ParsedFile -> Either LinkError (TentativeLoad, ([Label], [Label]))
+loadWithInt :: PageId -> ParsedFile -> Either LinkError (TentativeLoad, ([Label], [Label]))
 loadWithInt n (ils, (kues, xoks)) = do
  loaded <- toTentativeLoad (initialAddress + (fromIntegral n)*maxSize) ils
  return (loaded, (kues, xoks))
 
-assignInts :: ParsedFile -> Int -> (Int, ParsedFile)
-assignInts a@(ils, ([], xoks)) n = (0, a) -- no kue means main
+assignInts :: ParsedFile -> PageId -> (PageId, ParsedFile)
+assignInts a@(_, ([], _)) _ = (0, a) -- no kue means main
 assignInts a n = (n, a)
 
 
 
-data Program' = Program' {loads :: [TentativeLoad]}
+data Program' = Program' {loads :: M.Map PageId (TentativeLoad, ([Label], [Label]))}
 
 initialAddress :: Word32
 initialAddress = 0x14830000 -- just a random value with no meaning
@@ -47,8 +47,8 @@ linker :: [ParsedFile] -> Either LinkError Program
 linker [(ils, ([], []))] = Program <$> toTentativeLoad initialAddress ils
 linker _ = error "Linking multiple files is not yet implemented"
 
-resolveLabel :: Program -> Label -> Maybe Word32
-resolveLabel (Program ttl) label = let lt = labelTable ttl in M.lookup label lt
+resolveLabel :: Word32 -> Program -> Label -> Maybe Word32
+resolveLabel currentNX (Program ttl) label = let lt = labelTable ttl in M.lookup label lt
 
 readNX :: Program -> Word32 -> Maybe (Word32, Instruction)
 readNX (Program ttl) currentNX = M.lookup currentNX (tentativeAddressTable ttl)
