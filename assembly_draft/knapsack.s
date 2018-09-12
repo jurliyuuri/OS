fc71d25281abda789b02b222cd77bb34217b59a9
sozysozbot <sozysozbot@gmail.com>
2018-09-12 23:21:37 +0900

    演算をバラす

--- assembly_draft/knapsack.s
+++ assembly_draft/knapsack.s
@@ -7,15 +7,15 @@ rcx : value
 
 */
 
-	.globl	_knapsack 
+.globl	_knapsack 
 _knapsack:
   pushq %rbp
   movl %edi, %r10d
   movq %rcx, %r11
-  xorl %eax, %eax
+  movl $0, %eax
   pushq %rbx
   movl $101, %ecx
-  xorl %r9d, %r9d
+  movl $0, %r9d
   subq $696, %rsp
   leaq -112(%rsp), %rdi
   leaq 292(%rsp), %r8
@@ -23,25 +23,36 @@ _knapsack:
   leaq -112(%rsp), %rbx
   leal -1(%r10), %edi
 .L7:
-  xorl %eax, %eax
+  movl $0, %eax
 .L2:
   movl %eax, %r10d
   cmpl %eax, %esi
   jl .L12
-  movl (%rdx,%r9,4), %ecx
-  movl (%rbx,%rax,4), %ebp
+  movq %r9, %rcx
+  shlq $2, %rcx
+  movl (%rcx,%rdx), %ecx
+  movq %rax, %rbp
+  shlq $2, %rbp
+  movl (%rbx,%rbp), %ebp
   cmpl %eax, %ecx
   jg .L3
   subl %ecx, %r10d
-  movl (%r11,%r9,4), %ecx
+  movq %r9, %rcx
+  shlq $2, %rcx
+  movl (%rcx,%r11), %ecx
   movslq %r10d, %r10
-  addl (%rbx,%r10,4), %ecx
+  shlq $2, %r10
+  addl (%rbx,%r10), %ecx
   cmpl %ebp, %ecx
   cmovl %ebp, %ecx
-  movl %ecx, (%r8,%rax,4)
+  movq %rax, %r10
+  shlq $2, %r10
+  movl %ecx, (%r8,%r10)
   jmp .L4
 .L3:
-  movl %ebp, (%r8,%rax,4)
+  movq %rax, %r10
+  shlq $2, %r10
+  movl %ebp, (%r8,%r10)
 .L4:
   incq %rax
   jmp .L2
@@ -55,7 +66,9 @@ _knapsack:
   jmp .L7
 .L9:
   movslq %esi, %rsi
-  movl (%r8,%rsi,4), %eax
+  movq %rsi, %rax
+  shlq $2, %rax
+  movl (%rax,%r8), %eax
   addq $696, %rsp
   popq %rbx
   popq %rbp
